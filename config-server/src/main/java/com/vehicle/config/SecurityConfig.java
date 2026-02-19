ac079daa0d747310fd2a5a0e030e36a5b9055988
unknown <dasariravid145@gmail.com>
2026-02-19 18:30:04 +0530

    Vyxentra Systems added eureka server module

--- config-server/src/main/java/com/vehicle/config/SecurityConfig.java
+++ config-server/src/main/java/com/vehicle/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.vehicle.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
